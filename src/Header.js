546c9328094a26e31cac8731c2c2f1991ac43765
Kenji <kenji.arakaki93@gmail.com>
2020-12-26 11:11:37 -0300

    First commit

---
+++ src/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+
+const Header = () => {
+    return(
+        <>
+            <header>
+                <h1>Hola mundo</h1>
+                <button className="btn btn-primary">Press me</button>
+            </header>
+        </>
+    )
+}
+
+export default Header
