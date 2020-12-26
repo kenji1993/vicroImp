669284610d25a38f169076f1473aad81dc239ba4
Kenji <kenji.arakaki93@gmail.com>
2020-12-26 13:06:23 -0300

    Instalacion de bootstrap icons y creado del CartWidget

---
+++ src/components/CartWidget.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Cart } from 'react-bootstrap-icons'
+
+const CartWidget = () => {
+    return(
+        <Cart className="bi bi-cart4"/>
+    )
+}
+
+
+export default CartWidget
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import CartWidget from './CartWidget'
 
 
 const Navbar = () => {
@@ -14,6 +15,11 @@ const Navbar = () => {
                     </li>
                         <li className="nav-item">
                         <a className="nav-link" href="#">Chombas</a>
+                    </li>
+                </ul>
+                <ul>
+                    <li>
+                        <CartWidget/>
                     </li>
                 </ul>
             </nav>
