61c1c64fbc4cf1276bfd04e08020d3b0fb432220
Kenji <kenji.arakaki93@gmail.com>
2021-01-03 12:47:46 -0300

    Productos <= stock

--- src/components/ItemCount.js
+++ src/components/ItemCount.js
@@ -6,8 +6,11 @@ const ItemCount = () => {
     
     const [productos, setProductos] = useState(0)
 
+    const stock = 9
     const sumarProductos = () => {
+      if(productos <= stock -1){
         setProductos(productos +1)
+      }
     }
     const restarProductos = () => {
         setProductos(productos -1)
@@ -15,17 +18,22 @@ const ItemCount = () => {
     if(productos < 0) {
        return setProductos(0)
     }
+    
 
     return(
         <div className="card col-3 card-product">
             <img src={chomba} className="card-img-top" alt="imagen chomba" />
             <div className="card-body">
                 <h2 className="card-title">CHOMBA TOMMY</h2>
-                <div className="productCounter">
-                    <button type="button" onClick={ restarProductos } className="btn btn-outline-dark btn-lg">-</button>
-                    <p>{ productos }</p>
-                    <button type="button" onClick={ sumarProductos } className="btn btn-outline-dark btn-lg">+</button>
+                <p className="stock">Stock: {stock}</p>
+                <div className="productCounter row">
+                    <button type="button" onClick={ restarProductos } className="btn btn-outline-dark btn-lg col-3">-</button>
+                    <p className="col-4 displayCounter">{ productos }</p>
+                    <button type="button" onClick={ sumarProductos } className="btn btn-outline-dark btn-lg col-3">+</button>
+
+                    <button type="button" className="btn btn-outline-dark btn-lg mt-1">Agregar al carrito</button>
                 </div>
+                
             </div>
             
         </div>
